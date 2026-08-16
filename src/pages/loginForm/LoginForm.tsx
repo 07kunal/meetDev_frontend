6bf40ad87231b70bf7a54289fbd3aafeb7eb4a26
kunal <kunalgautam200@gmail.com>
2026-08-16 12:54:53 +0530

    Added the changes

--- src/pages/loginForm/LoginForm.tsx
+++ src/pages/loginForm/LoginForm.tsx
@@ -75,7 +75,7 @@ const LoginForm = () => {
             )}
             <h2 className="text-2xl font-bold text-center">Login</h2>
 
-            <form className="space-y-5" onSubmit={handleSubmit}>
+            <form className="space-y-3" onSubmit={handleSubmit}>
               {/* Email */}
               <div className="form-control">
                 <label className="label">
