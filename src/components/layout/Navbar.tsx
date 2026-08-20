f3ae70b77bd50c06ef49188062f8fe92a3e79a1d
kunal <kunalgautam200@gmail.com>
2026-08-20 22:38:05 +0530

    Remove unused code

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,20 +5,7 @@ import { useMutation} from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 import { useTokenExpiredMethod } from "../utils/customHooks/useTokenExpiredMethod";
-const Logo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="40" height="40">
-    <defs>
-      <linearGradient id="gradDM" x1="0%" y1="0%" x2="100%" y2="100%">
-        <stop offset="0%" stop-color="#3ABEF9" />
-        <stop offset="100%" stop-color="#7B2FF7" />
-      </linearGradient>
-    </defs>
-    <path d="M10 10h14c8 0 14 6 14 14s-6 14-14 14H10V10z" fill="url(#gradDM)" />
-    <path d="M30 10l8 14 8-14h8l-8 14 8 14h-8l-8-14-8 14h-8l8-14-8-14h8z" fill="url(#gradDM)" />
-    <rect x="50" y="6" width="6" height="6" rx="1" fill="#3ABEF9" />
-    <rect x="58" y="2" width="6" height="6" rx="1" fill="#7B2FF7" />
-  </svg>
-);
+
 const Navbar = () => {
   const userData: UserProfile = useAppSelector((state) => state?.user);
   const navigate = useNavigate();
